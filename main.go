6165101a592761e6e74c201969e869ae0d941647
Arturo Aguilar Lopez <aguilarlopez18@gmail.com>
2021-01-30 22:30:59 -0300

    change main

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ArturoAguilar1/bookstore_oauth-api/src/app"
+
+func main() {
+	app.StartApplication()
+}
--- src/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/ArturoAguilar1/bookstore_oauth-api/app"
-
-func main() {
-	app.StartApplication()
-}
